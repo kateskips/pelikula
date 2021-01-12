fee2afc1e6168a4d462cb48cbc58f533266c31c1
Kate Follett <kate.follett@baruchmail.cuny.edu>
2021-01-11 19:55:47 -0500

    Added title and movies class names

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import SearchMovies from './searchMovies';
+import SearchResults from './searchResults';
 import './App.css';
 
 const App = () => {
   return (
-    <div className="App">
+    <div className="container">
+      <h1 className="title">Pelikula</h1>
+      <SearchMovies />
+      <br></br>
+      <div className="movies">
+        <SearchResults />
+      </div>
     </div>
   );
 }
