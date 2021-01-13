2e5211675389e35054185ca19e958e2fa706a333
Kate Follett <kate.follett@baruchmail.cuny.edu>
2021-01-13 12:41:34 -0500

    adding state

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
-import React from 'react';
-import SearchMovies from './searchMovies';
-import SearchResults from './searchResults';
+import React, { useState, useEffect } from 'react';
+import SearchResults from './components/SearchResults';
 import './App.css';
 
 const App = () => {
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async () => {
+    const url = "http://www.omdbapi.com/?s=kingkong&apikey=8fc35db7"
+
+    const resp = await fetch(url)
+    const responseJson = await resp.json()
+
+    console.log(responseJson)
+    setMovies(responseJson.Search)
+  };
+  useEffect(() => {
+    getMovies()
+  }, []);
+
+
   return (
     <div className="container">
       <h1 className="title">Pelikula</h1>
-      <SearchMovies />
-      <br></br>
-      <div className="movies">
-        <SearchResults />
-      </div>
+      <h3 className="subheader">Movie Awards for Entrepreneurs, Con Artists, and Humans</h3>
+      <SearchResults movies={movies} />
     </div>
-  );
+  )
+
+
 }
 
+  
+    
+  
+
 export default App;
+ 
