baa7e71f186a3d383208410aa0024c6f3ca94265
Kate Follett <kate.follett@baruchmail.cuny.edu>
2021-01-14 18:14:23 -0500

    search form

---
+++ src/components/SearchForm.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const SearchForm = (props) => {
+  
+        return (
+            <>
+            <form className="form">
+                <label className="label" htmlFor="query">Movie Name</label>
+                    <input className="input" type="text" value={props.value}
+                        onChange={(event) => props.setSearchVal(event.target.value)}
+                        placeholder="i.e. King Kong"   
+                    />
+                <button className="button" type="submit">Search</button>
+            </form>
+            </>
+        )
+    }
+
+
+export default SearchForm;
