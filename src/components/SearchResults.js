7706ad10bb7b323dfddcc18fc6809c51dc676a1b
Kate Follett <kate.follett@baruchmail.cuny.edu>
2021-01-14 18:21:51 -0500

    updated search results with content

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,14 +1,25 @@
 import React from 'react';
 
 const SearchResults = (props) => {
+
     return (
-        <>
-        {props.movies.map((movie, index) => <div>
-            <img src={movie.Poster} alt='movie'></img>
-            </div>)
-            }
-        </>
+        <div className="card-list">
+                {props.movies.map(movie => (
+                    <div className="card" key={movie.imdbID}>
+                        <img className="card--image"
+                            src={movie.Poster} alt={movie.title + 'Poster'}
+                            />
+                        <div className="card--content">
+                        <h3 className="card--title">{movie.Title}</h3>
+                            <p><small>Release Date: {movie.Year}</small></p>
+                            <p className="card--desc">{movie.Plot}</p>
+                        </div>
+
+                    </div>
+                ))}
+            </div>    
     )
 }
 
 export default SearchResults;
+
