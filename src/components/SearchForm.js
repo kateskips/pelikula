a30e37592206fd86b9ea76bfca51e37fd08adb27
Kate Follett <kate.follett@baruchmail.cuny.edu>
2021-01-16 12:09:15 -0500

    comment out search button

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -3,16 +3,16 @@ import React from 'react';
 const SearchForm = (props) => {
   
         return (
-            <>
+            
             <form className="form">
                 <label className="label" htmlFor="query">Movie Name</label>
                     <input className="input" type="text" value={props.value}
                         onChange={(event) => props.setSearchVal(event.target.value)}
                         placeholder="Search a movie..."   
                     />
-                <button className="button" type="submit">Search</button>
+                    {/*<button className="button" type="submit">Search</button>*/}
             </form>
-            </>
+            
         )
     }
 
