00c957537aaae438d989682abd5cbbb161c95514
Kate Follett <kate.follett@baruchmail.cuny.edu>
2021-01-16 13:02:46 -0500

    created a AddNominations component button

---
+++ src/components/AddNominations.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddNominations = () => {
+    return (
+        <div className="nominate-button">
+        <button>Nominate</button>
+        </div> 
+    )
+}
+
+export default AddNominations;
