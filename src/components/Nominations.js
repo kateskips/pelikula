4e2b5224dd1f0ccbdd2414d29c670b836d62dff6
Kate Follett <kate.follett@baruchmail.cuny.edu>
2021-01-16 13:03:11 -0500

    created a Nominations component

---
+++ src/components/Nominations.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Nominations = (props) => {
+    return (
+        <div>
+            <h3>Nominations</h3>
+        </div>
+    )
+}
+
+export default Nominations;
